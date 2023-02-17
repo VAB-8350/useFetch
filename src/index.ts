99800c6ce5dfd2d372bae52416b2b224b11b0226
vabklb <v.barilin@itecriocuarto.org.ar>
2023-02-17 16:20:41 -0300

    Fix errors

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ export default function useFetch (instanceOptions: AxiosRequestConfig) {
 
     } catch (error: any) {
 
-      return formatResponse({error: error.response.message}, error.response.status)
+      return formatResponse({error: error.message}, error.response.status)
 
     }
 
@@ -45,7 +45,7 @@ export default function useFetch (instanceOptions: AxiosRequestConfig) {
 
     } catch (error: any) {
 
-      return formatResponse({error: error.response.message}, error.response.status)
+      return formatResponse({error: error.message}, error.response.status)
 
     }
 
@@ -61,7 +61,7 @@ export default function useFetch (instanceOptions: AxiosRequestConfig) {
 
     } catch (error: any) {
 
-      return formatResponse({error: error.response.message}, error.response.status)
+      return formatResponse({error: error.message}, error.response.status)
 
     }
 
@@ -77,7 +77,7 @@ export default function useFetch (instanceOptions: AxiosRequestConfig) {
 
     } catch (error: any) {
 
-      return formatResponse({error: error.response.message}, error.response.status)
+      return formatResponse({error: error.message}, error.response.status)
 
     }
 
